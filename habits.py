fcdbc00d2fc170756723adc86b2497a4793bc51d
Shechem <shechemsumanthiran@gmail.com>
2021-02-19 17:42:40 +0530

    Reworked to get details of the habit in item notes

--- habits.py
+++ habits.py
@@ -4,6 +4,7 @@ import logging
 from datetime import datetime
 from dateutil import tz
 from todoist.api import TodoistAPI
+from todoist.managers.notes import NotesManager
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -19,23 +20,85 @@ def get_token():
 
 
 class Task(object):
-    def __init__(self, item):
+    def __init__(self, api, item, notes, weekstart):
         self.item = item
+        self.id = self.item['id']
+        self.notes = notes
+        self.api = api
+        self.summary, self.week, self.streak = self.parse_notes()
+        self.weekstart = weekstart
 
-    def get_habit(self):
-        return re.search(r'\[day\s(\d+)\/(\d+)\]', self.item['content'])
+    def parse_notes(self):
+        """
+        Reads the relevant current notes and returns them
+        Adds the note if the note does not exist
+        """
+        summary = None
+        week = None
+        streak = None
+        for note in self.notes:
+            if note['content'].startswith('Summary:'):
+                summary = note
+            if note['content'].startswith('Weekly:'):
+                week = note
+            if note['content'].startswith('Streak:'):
+                streak = note
+        if summary is None:
+            summary = self.api.notes.add(self.id, 'Summary: 0/0 | 0%')
+        if week is None:
+            week = self.api.notes.add(self.id, 'Weekly: 0/0')
+        if streak is None:
+            streak = self.api.notes.add(self.id, 'Streak: 0 days')
 
-    def is_habit(self):
+        return summary, week, streak
+
+    def increase_streak(self):
+        streak = self.streak
+        res = re.search(r'(\d+)', streak['content'])
+        current_days = int(res.group(1))
+        days = '{}'.format(current_days + 1)
+        text = re.sub(r'(\d+)', days, streak['content'])
+        streak.update(content=text)
+        self.update_content(text)
+
+    def reset_streak(self):
+        streak = self.streak
+        days = '{}'.format(0)
+        text = re.sub(r'(\d+)', days, streak['content'])
+        streak.update(content=text)
+        self.update_content(text)
+    
+    def update_week(self, n = 1):
         """
-        Check if task is a habit task.
+        Increases the week note by n days
         """
-        habit = self.get_habit()
-        conditions = [
-            habit,  # Task has to be a habit pattern
-            self.item['due'].get('date') if self.item['due'] else None,  # Task has to have due date
-            not self.item['in_history']  # Task should not be archived
-        ]
-        return all(conditions)
+        week = self.week
+        res = re.search(r'(\d+)\/(\d+)', week['content'])
+        if self.weekstart:
+            days = '{}/{}'.format(n, 1)
+        else:
+            cur, tot = int(res.group(1)), int(res.group(2))
+            days = '{}/{}'.format(cur + n, tot + 1)
+        text = re.sub(r'(\d+)\/(\d+)', days, week['content'])
+        week.update(content=text)
+
+    def update_summary(self, n = 1):
+        """
+        Increases the summary note by n days
+        """
+        summary = self.summary
+        res = re.search(r'(\d+)\/(\d+)', summary['content'])
+        cur, tot = int(res.group(1)), int(res.group(2))
+        days = '{}/{}'.format(cur + n, tot + 1)
+        percentage = '{}%'.format(int(100.0*(cur + n)/(tot +1)))
+        text = re.sub(r'(\d+)\/(\d+)', days, summary['content'])
+        text = re.sub(r'(\d+)\%', percentage, text)
+        summary.update(content=text)
+
+    def update_content(self, content):
+        details = self.item['content'].split(' || ')[0]
+        text = details + ' || ' + content
+        self.item.update(content=text)
 
     @property
     def due_date(self):
@@ -51,62 +114,29 @@ class Task(object):
         """
         return today not in self.due_date
 
-    @property
-    def current_streak(self):
-        """
-        Parse and get current stream from the pattern.
-
-        Pattern: [day X]
-        :return: Current streak
-        """
-        habit = self.get_habit()
-        return [int(habit.group(1)), int(habit.group(2))]
-
-    def set_streak(self, streak):
-        """
-        Set streak for a task.
-        :param streak: Number of days
-        :return: None
-        """
-        days = '[day {}/{}]'.format(streak[0], streak[1])
-        text = re.sub(r'\[day\s(\d+)\/(\d+)\]', days, self.item['content'])
-        self.item.update(content=text)
-
     def increase(self):
         """
-        Increase streak by n days.
-        Default: 1 day
-        :param n: Number of days
+        Increase streak by 1 day.
+        Increase overall by 1 day.
+        Increase weekly by 1 day and reset on first day of the week
         """
-        streak = self.current_streak
-        streak[0] += 1
-        streak[1] += 1
-        self.set_streak(streak)
-
-    def decrease(self, today):
-        """
-        Decrease streak by 1 day.
-        Doesn't go to negative.
-        """
-        streak = max(0, self.current_streak - 1)
-        self.set_streak(streak)
-        self.item.update(due={'string': 'ev day starting {}'.format(today)})
-
-    def reset_to_zero(self, today):
-        """
-        Set streak to zero.
-        """
-        self.set_streak(0)
-        self.item.update_date_complete(due={'string': 'ev day',
-                                            'date': today})
+        self.increase_streak()
+        self.update_summary(n=1)
+        self.update_week(n=1)
     
-    def no_change(self):
+    def no_change(self, today):
         """
         Maintains current count and increases number of total days
         """
-        streak = self.current_streak
-        streak[1] += 1
-        self.set_streak(streak)
+        self.reset_streak()
+        self.update_summary(n=0)
+        self.update_week(n=0)
+        due_date = self.item['due'].get('date')
+        if 'T' in due_date:
+            time = due_date.split('T')[1]
+            today = today + 'T' + time
+        self.item.update_date_complete(due={'string': self.item['due'].get('string'),
+                                            'date': today})
         
 
 
@@ -114,29 +144,43 @@ class Todoist(object):
     def __init__(self):
         self.api = TodoistAPI(get_token())
         self.api.sync()
+        self.api.notes = NotesManager(self.api)
+        habit_label_ids = [label['id'] for label in self.api.state['labels'] if label['name']=='habit']
+        assert (len(habit_label_ids)==1)
+        self.habit_label_id = habit_label_ids[0]
+        self.habits = self.get_habits()
+        print (self.habits)
+        self.get_datetime()
+        
 
-    @property
-    def today(self):
+    def get_datetime(self):
         timezone = self.api.state['user']['tz_info']['timezone']
         tz_location = tz.gettz(timezone)
         now = datetime.now(tz=tz_location)
-        return now.strftime(TODOIST_DATE_FORMAT)
+        self.weekstart = datetime.weekday(now) == self.api.state['user']['start_day'] #Checks if yesterday was the week start day
+        self.today = now.strftime(TODOIST_DATE_FORMAT)
 
-    def update_streak(self):
-        items = self.api.state['items']
-        for item in items:
-            task = Task(item)
-            if task.is_habit():
-                if task.is_due(self.today):
-                    task.no_change()
-                else:
-                    task.increase()
+    def get_habits(self):
+        habits = []
+        for item in self.api.state['items']:
+            if self.habit_label_id in item['labels']:
+                habits.append(item)
+        return habits
+
+    def update_habit(self):
+        for item in self.habits:
+            notes = [note for note in self.api.state['notes'] if note['item_id'] == item['id']]
+            task = Task(self.api, item, notes, self.weekstart)
+            if task.is_due(self.today):
+                task.no_change(self.today)
+            else:
+                task.increase()
         self.api.commit()
 
 
 def main():
     todo = Todoist()
-    todo.update_streak()
+    todo.update_habit()
 
 
 if __name__ == '__main__':
